7864cfb32ee2d6e1a26a17ef78af26e80919900e
TH <tho@scarabaeus1033.net>
2026-03-03 22:36:01 +0100

    Update constant_propagation_demo.py

--- ENGINE/applications/constant_propagation_demo.py
+++ ENGINE/applications/constant_propagation_demo.py
@@ -11,9 +11,6 @@ from ENGINE.applications.constant_lattice import (
 
 
 def main():
-    # -------------------------------------------------
-    # Variables
-    # -------------------------------------------------
 
     variables = {"x", "y", "z"}
     constants = {1, 2}
@@ -21,45 +18,31 @@ def main():
     lattice = build_state_lattice(variables, constants)
     lat = LatticeOps(lattice)
 
-    # -------------------------------------------------
-    # CFG
-    # -------------------------------------------------
-
     nodes = {1, 2, 3}
     edges = {(1, 2), (2, 3)}
 
-    # -------------------------------------------------
-    # Initial state
-    # -------------------------------------------------
-
     bottom_atomic = ConstVal.bottom()
-    bottom_state = State({v: bottom_atomic for v in variables})
+    bottom_state = State(
+        frozenset((v, bottom_atomic) for v in variables)
+    )
 
     initial = {n: bottom_state for n in nodes}
 
-    # -------------------------------------------------
-    # Transfer functions
-    # -------------------------------------------------
-
     def transfer(node: int, state: State) -> State:
 
-    if node == 1:
-        return state.with_update("x", ConstVal.const(1))
+        if node == 1:
+            return state.with_update("x", ConstVal.const(1))
 
-    if node == 2:
-        return state.with_update("y", state.get("x"))
+        if node == 2:
+            return state.with_update("y", state.get("x"))
 
-    if node == 3:
-        y_val = state.get("y")
-        if not y_val.is_top and not y_val.is_bottom:
-            return state.with_update("z", ConstVal.const(y_val.value + 1))
-        return state.with_update("z", ConstVal.top())
+        if node == 3:
+            y_val = state.get("y")
+            if not y_val.is_top and not y_val.is_bottom:
+                return state.with_update("z", ConstVal.const(y_val.value + 1))
+            return state.with_update("z", ConstVal.top())
 
-    return state
-
-    # -------------------------------------------------
-    # Solve
-    # -------------------------------------------------
+        return state
 
     result = solve_worklist(
         nodes=nodes,
