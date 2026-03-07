ff3b40da2e3b79e58c2fd63b221dd07cbd74bc11
TH <tho@chairos.de>
2026-03-07 20:28:41 +0100

    upload

--- BUILDER_LAB/tests/test_regime_mapper.py
+++ BUILDER_LAB/tests/test_regime_mapper.py
@@ -17,21 +17,4 @@ def test_regime_mapper():
 
 
 if __name__ == "__main__":
-    test_regime_mapper()from FRAMEWORK.core.system_loader import load_system
-from FRAMEWORK.ARCHY.regime_mapper import map_regimes
-
-
-def test_regime_mapper():
-
-    system = load_system("APPLICATIONS/examples/energy_grid.json")
-
-    regime_map = map_regimes(system)
-
-    print("Collapse states:", regime_map["collapse_states"])
-    print("Basins:", regime_map["basins"])
-    print("Graph nodes:", regime_map["graph"].nodes())
-
-
-if __name__ == "__main__":
     test_regime_mapper()
-
