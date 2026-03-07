657802664ee6f1a23ff45f16f8895771d31b996b
TH <tho@scarabaeus1033.net>
2026-03-07 22:49:09 +0100

    Update system_explorer.py

--- FRAMEWORK/explorer/system_explorer.py
+++ FRAMEWORK/explorer/system_explorer.py
@@ -1,13 +1,21 @@
 from FRAMEWORK.MESO.collapse_basin import compute_collapse_basin
 from FRAMEWORK.MESO.visualize_collapse_basin import visualize_collapse_basin
+from FRAMEWORK.MESO.stability_atlas import compute_stability_atlas
+from FRAMEWORK.MESO.visualize_stability_atlas import visualize_stability_atlas
+from FRAMEWORK.MESO.attractor_detection import detect_attractors
+from FRAMEWORK.MESO.visualize_attractors import visualize_attractors
+
 from FRAMEWORK.core.system_loader import load_system
 from FRAMEWORK.ARCHY.regime_mapper import map_regimes
 from FRAMEWORK.ARCHY.visualize_regimes import visualize_regime_map
+
 from FRAMEWORK.MESO.risk_geometry import compute_risk_geometry
 from FRAMEWORK.MESO.visualize_risk_landscape import visualize_risk_landscape
 from FRAMEWORK.MESO.visualize_risk_labels import visualize_risk_labels
+
 from FRAMEWORK.NEXAH.navigation_policy import compute_safe_path
 from FRAMEWORK.NEXAH.visualize_navigation_path import visualize_navigation_path
+
 from FRAMEWORK.MEVA.execution_engine import ExecutionEngine
 
 
@@ -69,6 +77,71 @@ class SystemExplorer:
         """
         Visualize the collapse basin of the system.
         """
+
+        basin = compute_collapse_basin(self.regime_map)
+
+        visualize_collapse_basin(self.regime_map, basin)
+
+        return basin
+
+    def show_stability_atlas(self):
+        """
+        Compute and visualize the stability atlas.
+        """
+
+        atlas = compute_stability_atlas(
+            self.regime_map,
+            self.risk
+        )
+
+        visualize_stability_atlas(
+            self.regime_map,
+            atlas
+        )
+
+        return atlas
+
+    def show_attractors(self):
+        """
+        Detect and visualize system attractors.
+        """
+
+        attractors = detect_attractors(self.regime_map)
+
+        visualize_attractors(self.regime_map)
+
+        return attractors
+
+    def run_navigation(self, start_state):
+        """
+        Run the NEXAH navigation policy starting from a given state.
+        """
+
+        self.engine.set_initial_state(start_state)
+
+        def policy(state):
+
+            path = compute_safe_path(state, self.regime_map, self.risk)
+
+            if len(path) > 1:
+                return path[1]
+
+            return None
+
+        trajectory = self.engine.run(policy)
+
+        return trajectory
+
+    def print_risk(self):
+        """
+        Print MESO risk geometry.
+        """
+
+        print("\nRisk distance:")
+        print(self.risk["risk_distance"])
+
+        print("\nRisk gradient:")
+        print(self.risk["risk_gradient"])        """
 
         basin = compute_collapse_basin(self.regime_map)
 
