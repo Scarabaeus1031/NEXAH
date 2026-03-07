995bb085916b7f000b6d77486b632abd7b3e4c52
TH <tho@scarabaeus1033.net>
2026-03-07 15:24:13 +0100

    Update basin_transition_graph.py

--- ENGINE/analysis/basin_transition_graph.py
+++ ENGINE/analysis/basin_transition_graph.py
@@ -27,4 +27,49 @@ class BasinTransitionGraph:
 
                 a = self.basin_map[i, j]
                 b = self.basin_map[i + 1, j]
-                c
+                c = self.basin_map[i, j + 1]
+
+                if a != b:
+                    edges.add(tuple(sorted((a, b))))
+
+                if a != c:
+                    edges.add(tuple(sorted((a, c))))
+
+        return sorted(edges)
+
+    def plot(self, edges):
+        """
+        Plot basin transition graph.
+        """
+
+        G = nx.Graph()
+
+        for i in range(len(self.maxima)):
+            G.add_node(f"M{i}")
+
+        for a, b in edges:
+            G.add_edge(f"M{a}", f"M{b}")
+
+        pos = {}
+
+        for i, (x, y) in enumerate(self.maxima):
+            pos[f"M{i}"] = (x, y)
+
+        plt.figure(figsize=(9, 7))
+
+        nx.draw(
+            G,
+            pos,
+            with_labels=True,
+            node_size=900,
+            node_color="tomato",
+            font_color="white",
+            edge_color="black",
+            width=2
+        )
+
+        plt.title("Basin Transition Graph")
+        plt.xlabel("Axis X")
+        plt.ylabel("Axis Y")
+
+        plt.show()
