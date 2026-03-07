ad49a73bac1ec16aed6793bef517f27ea00e8296
TH <tho@scarabaeus1033.net>
2026-03-07 23:35:45 +0100

    Update system_explorer.py

--- FRAMEWORK/explorer/system_explorer.py
+++ FRAMEWORK/explorer/system_explorer.py
@@ -6,6 +6,7 @@ from FRAMEWORK.MESO.stability_atlas import compute_stability_atlas
 from FRAMEWORK.MESO.visualize_stability_atlas import visualize_stability_atlas
 from FRAMEWORK.MESO.attractor_detection import detect_attractors
 from FRAMEWORK.MESO.visualize_attractors import visualize_attractors
+from FRAMEWORK.MESO.visualize_tipping_points import visualize_tipping_points
 
 from FRAMEWORK.core.system_loader import load_system
 from FRAMEWORK.ARCHY.regime_mapper import map_regimes
@@ -24,52 +25,109 @@ from FRAMEWORK.MEVA.execution_engine import ExecutionEngine
 class SystemExplorer:
 
     def __init__(self, system_file):
+
         self.system = load_system(system_file)
+
         self.regime_map = map_regimes(self.system)
+
         self.risk = compute_risk_geometry(self.regime_map)
+
         self.engine = ExecutionEngine(self.regime_map, self.risk)
 
+
     def show_graph(self):
+
         visualize_regime_map(self.regime_map)
 
+
     def show_risk_landscape(self):
+
         visualize_risk_landscape(self.regime_map, self.risk)
 
+
     def show_risk_labels(self):
+
         visualize_risk_labels(self.regime_map, self.risk)
 
+
     def show_navigation(self, start_state):
+
         path = compute_safe_path(start_state, self.regime_map, self.risk)
+
         visualize_navigation_path(self.regime_map, path)
+
         return path
 
+
     def show_collapse_basin(self):
+
         basin = compute_collapse_basin(self.regime_map)
+
         visualize_collapse_basin(self.regime_map, basin)
+
         return basin
 
-    def show_stability_atlas(self):
-        atlas = compute_stability_atlas(self.regime_map, self.risk)
-        visualize_stability_atlas(self.regime_map, atlas)
-        return atlas
-
-    def show_attractors(self):
-        attractors = detect_attractors(self.regime_map)
-        visualize_attractors(self.regime_map)
-        return attractors
 
     def show_stability_landscape(self):
+
         landscape = compute_stability_landscape(self.regime_map, self.risk)
+
         visualize_stability_landscape(self.regime_map, landscape)
+
         return landscape
 
+
+    def show_stability_atlas(self):
+
+        atlas = compute_stability_atlas(self.regime_map, self.risk)
+
+        visualize_stability_atlas(self.regime_map, atlas)
+
+        return atlas
+
+
+    def show_attractors(self):
+
+        attractors = detect_attractors(self.regime_map)
+
+        visualize_attractors(self.regime_map)
+
+        return attractors
+
+
+    def show_tipping_points(self):
+
+        visualize_tipping_points(
+            self.regime_map,
+            self.risk
+        )
+
+
     def run_navigation(self, start_state):
+
         self.engine.set_initial_state(start_state)
 
         def policy(state):
+
             path = compute_safe_path(state, self.regime_map, self.risk)
+
             if len(path) > 1:
                 return path[1]
+
+            return None
+
+        trajectory = self.engine.run(policy)
+
+        return trajectory
+
+
+    def print_risk(self):
+
+        print("\nRisk distance:")
+        print(self.risk["risk_distance"])
+
+        print("\nRisk gradient:")
+        print(self.risk["risk_gradient"])                return path[1]
             return None
 
         return self.engine.run(policy)
