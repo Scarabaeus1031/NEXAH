edd7d903a9505c91d0181b5f058eeff077c90d96
TH <tho@scarabaeus1033.net>
2026-03-17 00:50:57 +0100

    Update __init__.py

---
+++ nexah/__init__.py
@@ -0,0 +1,38 @@
+"""
+NEXAH Public API
+
+This module exposes the core structural components of the NEXAH Engine
+for external users.
+
+Example:
+
+    from nexah import Poset, Lattice
+"""
+
+# Core algebraic structures
+
+from ENGINE.core.poset import FinitePoset as Poset
+from ENGINE.core.lattice import LatticeOps as Lattice
+
+# Operators
+
+from ENGINE.core.closure_operator import ClosureOperator
+from ENGINE.core.interior_operator import InteriorOperator
+from ENGINE.core.monotone_operator import MonotoneOperator
+from ENGINE.core.frame_operator import FrameOperator
+from ENGINE.core.regime_operator import RegimeOperator
+
+# Fixpoint systems
+
+from ENGINE.core.fixpoint_lattice import FixpointSolver
+
+__all__ = [
+    "Poset",
+    "Lattice",
+    "ClosureOperator",
+    "InteriorOperator",
+    "MonotoneOperator",
+    "FrameOperator",
+    "RegimeOperator",
+    "FixpointSolver",
+]
