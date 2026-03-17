db0c18e435c78ca417feb4770a1a9bf97780f710
TH <tho@scarabaeus1033.net>
2026-03-17 01:08:47 +0100

    Update __init__.py

--- nexah/__init__.py
+++ nexah/__init__.py
@@ -9,12 +9,12 @@ Example:
     from nexah import Poset, Lattice
 """
 
-# Core algebraic structures
+# --- Core algebraic structures ---
 
 from ENGINE.core.poset import FinitePoset as Poset
 from ENGINE.core.lattice import LatticeOps as Lattice
 
-# Operators
+# --- Operators ---
 
 from ENGINE.core.closure_operator import ClosureOperator
 from ENGINE.core.interior_operator import InteriorOperator
@@ -22,9 +22,12 @@ from ENGINE.core.monotone_operator import MonotoneOperator
 from ENGINE.core.frame_operator import FrameOperator
 from ENGINE.core.regime_operator import RegimeOperator
 
-# Fixpoint systems
+# --- Fixpoint systems ---
 
 from ENGINE.core.fixpoint_lattice import FixpointSolver
+
+
+# Public API export list
 
 __all__ = [
     "Poset",
