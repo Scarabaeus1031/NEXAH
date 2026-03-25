b30878103a242a5c7cfff148ca87ecc0926276bd
TH <tho@scarabaeus1033.net>
2026-03-25 01:12:23 +0100

    Update stability_scan.py

--- APPLICATIONS/power_systems/ieee_test_cases/stability_scan.py
+++ APPLICATIONS/power_systems/ieee_test_cases/stability_scan.py
@@ -1,4 +1,5 @@
 import pandapower as pp
+import copy
 
 def run_stability_scan(net, min_factor=1.0, max_factor=2.0, steps=20):
     results = []
@@ -6,7 +7,9 @@ def run_stability_scan(net, min_factor=1.0, max_factor=2.0, steps=20):
     for i in range(steps + 1):
         factor = min_factor + (max_factor - min_factor) * i / steps
 
-        net_copy = net.deepcopy()
+        # FIX: pandapowerNet hat kein .deepcopy()
+        net_copy = copy.deepcopy(net)
+
         net_copy.load["p_mw"] *= factor
 
         try:
