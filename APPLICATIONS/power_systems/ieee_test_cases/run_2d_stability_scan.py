4d133d5056014b714b02296882dd7cc829363a11
TH <tho@scarabaeus1033.net>
2026-03-25 01:51:49 +0100

    Update and rename stability_landscape.py to run_2d_stability_scan.py

--- APPLICATIONS/power_systems/ieee_test_cases/run_2d_stability_scan.py
+++ APPLICATIONS/power_systems/ieee_test_cases/run_2d_stability_scan.py
@@ -2,14 +2,13 @@ import pandapower as pp
 import copy
 import numpy as np
 
-
 def run_2d_stability_scan(
     net,
     bus_a,
     bus_b,
-    min_factor=1.0,
-    max_factor=4.5,
-    steps=30
+    min_factor=3.8,
+    max_factor=4.3,
+    steps=40
 ):
     factors = np.linspace(min_factor, max_factor, steps)
     landscape = np.zeros((steps, steps))
@@ -19,18 +18,20 @@ def run_2d_stability_scan(
 
             net_copy = copy.deepcopy(net)
 
-            # Masken für Load-Busse
+            # 🔥 WICHTIG: globales Stress-Level
+            net_copy.load["p_mw"] *= 3.5
+
+            # 🔥 lokale Variation
             mask_a = net_copy.load["bus"] == bus_a
             mask_b = net_copy.load["bus"] == bus_b
 
-            # Load skalieren
-            net_copy.load.loc[mask_a, "p_mw"] *= fa
-            net_copy.load.loc[mask_b, "p_mw"] *= fb
+            net_copy.load.loc[mask_a, "p_mw"] *= fa / 3.5
+            net_copy.load.loc[mask_b, "p_mw"] *= fb / 3.5
 
             try:
                 pp.runpp(net_copy)
-                landscape[i, j] = 1  # stable
+                landscape[i, j] = 1
             except Exception:
-                landscape[i, j] = 0  # unstable
+                landscape[i, j] = 0
 
     return factors, landscape
