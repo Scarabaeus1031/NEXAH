da73abfea0bd338a5331706544cf3bd4bbc2c77b
TH <tho@scarabaeus1033.net>
2026-03-26 21:37:17 +0100

    Update run_scan_v26_phase_transition_map.py

--- APPLICATIONS/power_systems/stability_field_dynamics/ieee_test_cases/run_scan_v26_phase_transition_map.py
+++ APPLICATIONS/power_systems/stability_field_dynamics/ieee_test_cases/run_scan_v26_phase_transition_map.py
@@ -1,6 +1,10 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(file), "../../../..")))
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../../../..")
+    )
+)
 
 import numpy as np
 import pandas as pd
