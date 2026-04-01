f4ee0710e9a03e7759234cc822fcddaf250c1ce7
TH <tho@scarabaeus1033.net>
2026-04-01 15:49:49 +0200

    Create ieee_loader_v118.py

---
+++ APPLICATIONS/power_systems/stability_field_dynamics/ieee_application/scripts/ieee_loader_v118.py
@@ -0,0 +1,13 @@
+import pandapower.networks as pn
+
+def load_ieee14():
+    return pn.case14()
+
+def load_ieee30():
+    return pn.case30()
+
+def load_ieee57():
+    return pn.case57()
+
+def load_ieee118():
+    return pn.case118()
