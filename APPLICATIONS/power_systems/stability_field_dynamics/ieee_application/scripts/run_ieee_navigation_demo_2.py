9d0d7ccb5b33bac10974db48ca3f4efd21e8711c
TH <tho@scarabaeus1033.net>
2026-04-01 16:28:13 +0200

    Update run_ieee_navigation_demo_2.py

--- APPLICATIONS/power_systems/stability_field_dynamics/ieee_application/scripts/run_ieee_navigation_demo_2.py
+++ APPLICATIONS/power_systems/stability_field_dynamics/ieee_application/scripts/run_ieee_navigation_demo_2.py
@@ -2,7 +2,8 @@ import sys
 import os
 
 # Füge den Wurzelordner zu sys.path hinzu
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))  # Wurzelverzeichnis hinzufügen
+sys.path.append(root_dir)
 
 # Nun den Import durchführen
 from APPLICATIONS.power_systems.stability_field_dynamics.ieee_application.scripts.ieee_loader_v118 import load_ieee118
