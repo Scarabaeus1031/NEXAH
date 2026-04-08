3d0840f2796fe4be8700827872f8f9d9468ac39e
TH <tho@scarabaeus1033.net>
2026-04-08 14:13:22 +0200

    Create __init__.py

---
+++ nexah/spiral_coupling/__init__.py
@@ -0,0 +1,11 @@
+# nexah/spiral_coupling/__init__.py
+
+"""
+Spiral Coupling Layer – Dreifache Spiralüberlagerung (Water-Mercury-Ferro)
+"""
+
+from .spiral_coupling_layer import SpiralCouplingLayer
+from .spiral_coupling_kernel import SpiralCouplingKernel
+
+__version__ = "0.1.0"
+__all__ = ["SpiralCouplingLayer", "SpiralCouplingKernel"]
